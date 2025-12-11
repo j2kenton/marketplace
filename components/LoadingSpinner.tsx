3f59d6112667ce2edcffc5c5c6a6f090802ad737
Jonathan Kenton <jonathankenton@gmail.com>
2025-12-11 21:35:48 +0200

    feat: enhance LoadingSpinner component to accept dynamic size prop

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,9 +1,17 @@
 import { ActivityIndicator, View } from "react-native";
 
-const LoadingSpinner = () => {
+const DEFAULT_SIZE = "small";
+
+interface Props {
+  size?: number | "small" | "large" | undefined;
+}
+
+const LoadingSpinner = (props: Props) => {
+  const { size = DEFAULT_SIZE } = props;
+
   return (
     <View>
-      <ActivityIndicator size="large" />
+      <ActivityIndicator size={size} />
     </View>
   );
 };

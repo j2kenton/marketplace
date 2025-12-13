552c0088bb45cadac231df031986e27cf494f94c
Jonathan Kenton <jonathankenton@gmail.com>
2025-12-14 00:25:33 +0200

    feat: add TODO comment to move handlePlaceOrder function to API service

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -112,6 +112,7 @@ function CartContent() {
     router.push({ pathname: "/product/[id]", params: { id: productId } });
   };
 
+  // TODO: move to api service
   const handlePlaceOrder = async () => {
     try {
       setLoading(true);
